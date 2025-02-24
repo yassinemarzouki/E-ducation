fc08259089d19142088b79a7331a2f4716757f5e
yassinemarzouki <92584248+yassinemarzouki@users.noreply.github.com>
2025-02-24 13:57:08 +0100

    Mise a jour Style Global

--- src/app/components/courses/courses.component.ts
+++ src/app/components/courses/courses.component.ts
@@ -1,21 +1,41 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Course } from '../../models/course.model';
+import { FormsModule } from '@angular/forms';
 import { CourseService } from '../../services/course.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent implements OnInit {
   courses: any[] = [];
+  searchText: string = ''; 
+  selectedCourse: any = null; 
+  newCourseTitle: string = ''; 
 
   constructor(private courseService: CourseService) {}
 
   ngOnInit(): void {
-    this.courses = this.courseService.getCourses(); // Récupère les cours
+    this.courses = this.courseService.getCourses();
+  }
+
+  register(course: any) {
+    console.log(`Inscription au cours : ${course.title}`);
+  }
+
+  selectCourse(course: any) {
+    this.selectedCourse = course;
+  }
+
+  addCourse() {
+    if (this.newCourseTitle) {
+      this.courses.push({ id: this.courses.length + 1, title: this.newCourseTitle, description: 'Nouveau cours ajouté', image: 'assets/images/default.jpg' });
+      this.newCourseTitle = ''; 
+    }
   }
 }
+
