32455ef60d9401c11691f8ec71d0a834b2dc32e0
yassinemarzouki <92584248+yassinemarzouki@users.noreply.github.com>
2025-02-26 18:08:09 +0100

    changement d'organisation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,13 @@ import { Routes } from '@angular/router';
 
 
 export const routes: Routes = [
-    
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent) },
+  { path: 'courses', loadComponent: () => import('./features/courses/course-list.component').then(m => m.CourseListComponent) },
+  { path: 'course/:id', loadComponent: () => import('./features/courses/course-detail.component').then(m => m.CourseDetailComponent) },
+  { path: 'dashboard', loadComponent: () => import('./features/dashboard/user-dashboard.component').then(m => m.UserDashboardComponent) },
+  { path: 'admin', loadComponent: () => import('./features/admin/admin-dashboard.component').then(m => m.AdminDashboardComponent) },
+  { path: 'auth', loadComponent: () => import('./features/auth/auth.component').then(m => m.AuthComponent) },
+  { path: '**', redirectTo: '/home' } // Gestion des erreurs 404
     
 ];
